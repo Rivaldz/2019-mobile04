2dae52e8208df0f2ac237520b947f7a1fa776f07
Rivaldz <rivalzsetyo@gmail.com>
2019-09-16 14:43:18 +0700

    pertanyaan : melengkapi ProfileBundleActivity

--- Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
@@ -3,6 +3,7 @@ package id.ac.polinema.intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class ProfileBundleActivity extends AppCompatActivity {
@@ -11,14 +12,20 @@ public class ProfileBundleActivity extends AppCompatActivity {
     private TextView nameText;
     private TextView ageText;
 
+    private EditText outputUsename;
+    private EditText outputName;
+    private EditText outputAge;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_bundle);
         // TODO: bind here
-        
-
+        usernameText = findViewById(R.id.text_username);
+        nameText = findViewById(R.id.text_name);
+        ageText = findViewById(R.id.text_age);
         Bundle extras = getIntent().getExtras();
+
         if (extras != null) {
             // TODO: display value here
         }
