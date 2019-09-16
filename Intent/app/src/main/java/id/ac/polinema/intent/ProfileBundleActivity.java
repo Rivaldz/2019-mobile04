2e40f494eca55d397d1f1e10f6cda6fd6f02ff66
Rivaldz <rivalzsetyo@gmail.com>
2019-09-16 14:33:01 +0700

    android bundle

--- Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
@@ -3,15 +3,20 @@ package id.ac.polinema.intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProfileBundleActivity extends AppCompatActivity {
+
+    private TextView usernameText;
+    private TextView nameText;
+    private TextView ageText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_bundle);
-
         // TODO: bind here
+        
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
